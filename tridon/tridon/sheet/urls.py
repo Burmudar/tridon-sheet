330f62008536b5c16e56165353730190b52646f0
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2016-08-29 19:01:20 +0200

    Add missing pages. Start with Invoice page combining file upload and entry display

--- tridon/tridon/sheet/urls.py
+++ tridon/tridon/sheet/urls.py
@@ -13,6 +13,11 @@ urlpatterns = [
         name='index'
     ),
     url(
+        regex=r'^invoice/$',
+        view=views.invoice,
+        name='invoice'
+    ),
+    url(
         regex=r'^upload/$',
         view=views.sheet_upload,
         name='upload'
@@ -20,6 +25,26 @@ urlpatterns = [
     url(
         regex=r'^(?P<workbook_pk>[0-9]+)/view/$',
         view=views.SheetEntryView.as_view(),
-        name="entries"
+        name='entries'
+    ),
+    url(
+        regex=r'^documents/$',
+        view=views.documents,
+        name='documents'
+    ),
+    url(
+        regex=r'^about/$',
+        view=views.about,
+        name='about'
+    ),
+    url(
+        regex=r'^contact/$',
+        view=views.contact,
+        name='contact'
+    ),
+    url(
+        regex=r'^invoice/(?P<pk>[0-9]*)$',
+        view=views.SheetDetailView.as_view(),
+        name='invoice'
     ),
 ]
--- tridon/tridon/sheet/views.py
+++ tridon/tridon/sheet/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.views.generic import ListView
+from django.views.generic.detail import SingleObjectMixin
 from .forms import WorkbookFileForm
 from .processors import WorkbookProcessor
 from .models import WorkbookEntry
@@ -24,7 +24,23 @@ def sheet_upload(request):
 
 
 def index(request):
-    return HttpResponse("Hello, world. YOu're at the sheet index.")
+    return render(request, 'sheet/index.html')
+
+
+def documents(request):
+    return render(request, 'sheet/documents.html')
+
+
+def about(request):
+    return render(request, 'sheet/about.html')
+
+
+def contact(request):
+    return render(request, 'sheet/contact.html')
+
+
+def invoice(request):
+    pass
 
 
 class SheetEntryView(ListView):
@@ -32,5 +48,25 @@ class SheetEntryView(ListView):
     context_object_name = "workbook_entries"
 
     def get_queryset(self):
+        if 'workbook_pk' in self.kwargs and self.kwargs['workbook']:
+            pass
         self.workbook = get_object_or_404(WorkbookFile, id=self.kwargs['workbook_pk'])
         return WorkbookEntry.objects.filter(workbook_file=self.workbook)
+
+
+class SheetDetailView(ListView, SingleObjectMixin):
+    template_name = "sheet/invoice.html"
+    context_object_name = "entries"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=WorkbookFile.objects.all())
+        return super(SheetDetailView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(SheetDetailView, self).get_context_data(**kwargs)
+        context['workbooks'] = WorkbookFile.objects.all()
+        context['workbook'] = self.object
+        return context
+
+    def get_queryset(self):
+        return self.object.workbookentry_set.all()
